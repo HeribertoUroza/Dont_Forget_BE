2a5522fe9eb9c30dbc2def8aeea7cd516c47688d
Heriberto Uroza <heribertouroza@pursuit.org>
2019-12-09 12:29:20 -0500

    created service function for list items, sql working

---
+++ services/list_itemS.js
@@ -0,0 +1,20 @@
+//LOCAL MODULES
+const { getDbConnection } = require('./dbConnection/db');
+const { dbAddress } = require('./dbConnection/config');
+
+//LIST ITEMS FUNCTIONS
+
+    //CREATE LIST ITEMS
+const postReqListItems = (list_item_name, category_id, is_completed) => getDbConnection(dbAddress).oneOfNone(
+    `
+        INSERT INTO list_items
+            (list_item_name, category_id, is_completed)
+        VALUES
+            ($[list_item_name], $[category_id], $[is_completed])
+        RETURNING list_item_id
+    `, { list_item_name, category_id, is_completed}
+)
+
+module.exports = {
+    postReqListItems,
+}
